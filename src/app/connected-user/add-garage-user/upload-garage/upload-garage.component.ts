f0eff3e212ccbc3620976afa88ece7d1ef2f9ed6
Mabilon Christophe <70352608+christophe-mabilon@users.noreply.github.com>
2021-09-19 18:02:21 +0200

    mise a jour photos et mise a jour formulaire de recherche simple

---
+++ src/app/connected-user/add-garage-user/upload-garage/upload-garage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-garage',
+  templateUrl: './upload-garage.component.html',
+  styleUrls: ['./upload-garage.component.scss']
+})
+export class UploadGarageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/newnavbar/newnavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newnavbar',
+  templateUrl: './newnavbar.component.html',
+  styleUrls: ['./newnavbar.component.scss']
+})
+export class NewnavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
